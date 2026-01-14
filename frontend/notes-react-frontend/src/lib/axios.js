6d7c04c388734ccb84ae867c291424ca03eb378f
Parth Tyagi <tyi.parth74@gmail.com>
2026-01-15 02:01:27 +0530

    Bug Fixed: Changed NODE_ENV to PROD in axios file under lib directory

--- frontend/notes-react-frontend/src/lib/axios.js
+++ frontend/notes-react-frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BaseURL = import.meta.env.NODE_ENV === "production" ? "/api" : "http://localhost:5001/api"
+const BaseURL = import.meta.env.PROD ? "/api" : "http://localhost:5001/api"
 
 const axiosApi = axios.create({
     baseURL: BaseURL
